9b2c3c9f2f6cf8189af37d385c15030acba3cc3e
jaramill0 <hugo98jaramillo@gmail.com>
2022-07-05 16:35:10 -0500

    componente App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Navbar from './components/Navbar';
+import Characters from './components/Characters';
+import Pagination from "./components/Pagination";
 
 function App() {
+
+  const [characters, setCharacter] = useState([]);
+
+  const [info, setInfo] = useState({});
+
+  //implementacion de WebApi de Rick & Morty
+  const initialurl = "https://rickandmortyapi.com/api/character";
+
+  //promesas y respuesta en la consola del navegador
+  const fectCharacters = (url) => {
+    fetch(url)
+      .then(Response => Response.json())
+      .then(data => {
+        setCharacter(data.results);
+        setInfo(data.info);
+      })
+      .catch(error => console.log(error))
+  };
+
+  //visualizar anteriores personajes
+  const onPrevious = () => {
+    fectCharacters(info.prev);
+  }
+  //funcion para ver mas personajes
+  const onNext = () => {
+    fectCharacters(info.next);
+  }
+  //llamando al paramentro para el Fetch 
+  useEffect(() => {
+    fectCharacters(initialurl);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar brand="Rick & Morty" />
+      <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext} />
+      <div className="container mt-5">
+        <Characters characters={characters} />
+      </div>
+    </>
   );
 }
 
